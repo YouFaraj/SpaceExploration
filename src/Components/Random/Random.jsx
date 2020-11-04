0e42be19306c789b9fab969e1bdb06e6a8e93179
Yousif Faraj <yfaraj96@gmail.com>
2020-11-04 15:16:47 -0500

    remove extra comments in Random

--- src/Components/Random/Random.jsx
+++ src/Components/Random/Random.jsx
@@ -11,14 +11,20 @@ const Random = () => {
   // helper functions
   const updateTerm = (e) => setTerm(e.target.value);
   const updateSearchTerm = (e) => setSearchTerm(term);
+  // easter egg
+  const addDeadPool = (searchTerm, component) => {
+    if(searchTerm === "") return ""
+    if(searchTerm === "chimichanga") return <img src="https://i.pinimg.com/564x/f3/04/01/f30401330019fb0d05cb39a281c2441a.jpg" alt="" />
+    return component
+  }
 
   return (
     <div className={styles.container}>
       <label className={styles.label}>
-        <span className={styles.search}>Search: </span><input className={styles.input} placeholder="Insert a search term" onChange={updateTerm} value={term} type="text" />
+        <span className={styles.search}>Search: </span><input className={styles.input} placeholder="chimichanga" onChange={updateTerm} value={term} type="text" />
       <button className={styles.button} type="submit" onClick={updateSearchTerm}>Take Off!</button><br></br>
       </label>
-      {searchTerm === ''? "" : <CardList term={searchTerm} />}
+      {addDeadPool(searchTerm, <CardList term={searchTerm} />)}
     </div>
   )
 }
